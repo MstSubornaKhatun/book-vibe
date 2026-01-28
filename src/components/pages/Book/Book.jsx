b4a23dd8bc99699bc21cc8c59339dccf802f7a0c
suborna33 <152749142+suborna33@users.noreply.github.com>
2026-01-28 12:42:02 +0600

    polishing caard

--- src/components/pages/Book/Book.jsx
+++ src/components/pages/Book/Book.jsx
@@ -1,44 +1,39 @@
-// import React, { use } from 'react';
 
-// const Book = ({booksPromise}) => {
-
-//     const data = use(booksPromise);
-//     console.log(data); // (9) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
-//     return (
-//         <div>
-            
-//         </div>
-//     );
-// };
-
-// export default Book;
 
 
 import React from 'react';
+import { FaStarHalfAlt } from 'react-icons/fa';
 
 const Book = ({singleBook}) => {
     console.log(singleBook);
 
     //distaring
-    const {bookName, author, image, review} = singleBook;
+    const {bookName, author, image, category,rating} = singleBook;
 
     return (
-       <div className="card bg-base-100 w-96 shadow-sm">
-  <figure className='p-3'>
-    <img
-        className=''
+       <div className="card bg-base-100 w-96 shadow-sm border">
+  <figure className='p-3 bg-gray-100 w-2/3 mx-auto'>
+    <img 
+        className='h-[166px]'
       src={image}
       alt="Shoes" />
   </figure>
   <div className="card-body">
+    <p className='flex space-x-3'>
+        <span className='text-[#23BE0A] bg-[#23BE0A]/10 p-1 rounded-2xl px-3'>Young Adult</span>
+        <span className='text-[#23BE0A] bg-[#23BE0A]/10 p-1 rounded-2xl px-3'>Identity</span>
+    </p>
     <h2 className="card-title">
       {bookName}
-      <div className="badge badge-secondary">NEW</div>
+      
     </h2>
-    <p>{review}</p>
+    <p>By: {author}</p>
+
+    <hr />
     <div className="card-actions justify-end">
-      <div className="badge badge-outline">Fashion</div>
-      <div className="badge badge-outline">Products</div>
+      <div className="  badge badge-outline ">{category}</div>
+      <div className="badge badge-outline">{rating}<FaStarHalfAlt />
+</div>
     </div>
   </div>
 </div>
