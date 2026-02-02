e2a57ef89be2a13080cf5af53502bbd836794c28
suborna33 <152749142+suborna33@users.noreply.github.com>
2026-02-02 19:17:00 +0600

    sweetalert and toastify

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -1,62 +1,57 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router';
 import { addToStoredDB } from '../../utility/addToDB';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+import { ToastContainer, toast } from 'react-toastify';
+  
+
+const MySwal = withReactContent(Swal);
 
 const BookDetails = () => {
-    // const id = useParams();
-    const {id} = useParams(); // useParams() kno use korlam note a bekkha korba
-    console.log(id)
+    const {id} = useParams(); 
     const bookId = parseInt(id);
 
-    const data = useLoaderData(); // useLoaderData(); kno use korlam note a bekkha korba
-    console.log(data);
-
-    const singleBook = data.find(book=>book.bookId === bookId);
-    console.log(singleBook)
-    console.log(typeof id, data);
+    const data = useLoaderData(); 
+    const singleBook = data.find(book => book.bookId === bookId);
 
     const {bookName, image} = singleBook || {}; 
 
-
     const handleMarkRead = id => {
-        //  Store with Id
-        // where to store
-        // array or collection
-        // if book already exist  the show a  alart
-        //  if book not exist then push in the collection or array
+        addToStoredDB(id);
 
+        // // SweetAlert after storing
+        // MySwal.fire({
+        //     title: 'Marked as Read!',
+        //     text: `${bookName} has been added to your Read list.`,
+        //     icon: 'success',
+        //     confirmButtonText: 'OK'
+        // });
+        toast("Wow so easy")
 
-        addToStoredDB(id)
-    }
+    };
+
+   
 
     return (
         <div>
             <div className='w-2/3 mx-auto'>
                 <img className='w-48' src={image} alt="" />
                 <h1>{bookName}</h1>
-                <button onClick={()=>handleMarkRead(id)} className='btn btn-outline'>Read</button>
-                <button className='btn btn-primary'>Wishlist</button>
-
+                <ToastContainer/>
+                <button 
+                    onClick={() => handleMarkRead(id)} 
+                    className='btn btn-outline'
+                >
+                    Read
+                </button>
+                <button 
+                     
+                    className='btn btn-primary'
+                >
+                    Wishlist
+                </button>
             </div>
-            {/* 
-            <div>
-            author, publisher, rating, yearOfPublishing, category, review, tags, totalPages
-                <p>By: {author}</p>
-                <hr />
-                <p>{category}</p>
-                <hr />
-                <p>Review: {review}</p>
-                <p>Tag: {
-                    tags.map(tag=><p>{tag}</p>)
-                    }</p>
-                    <hr />
-                <p>Number of pages: {totalPages}</p> 
-                <p>Publisher: {publisher}</p>   
-                <p>Year of Publishing: {yearOfPublishing}</p>
-                <p>Rating: {rating}</p>
-            </div> */}
-
-
         </div>
     );
 };
